00b4c0544616adfc8347cc216634773540dd3352
MatviiU <matveyulitskiy@gmail.com>
2025-09-21 20:19:10 +0200

    feat(notes): implemented BLoC for notes management

---
+++ lib/features/notes/presentation/bloc/notes_event.dart
@@ -0,0 +1,47 @@
+part of 'notes_bloc.dart';
+
+
+sealed class NotesEvent extends Equatable {
+  const NotesEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class LoadNotes extends NotesEvent {}
+
+final class NotesUpdated extends NotesEvent {
+  const NotesUpdated(this.notes);
+
+  final List<Note> notes;
+
+  @override
+  List<Object?> get props => [notes];
+}
+
+final class AddNoteRequested extends NotesEvent {
+  const AddNoteRequested(this.note);
+
+  final Note note;
+
+  @override
+  List<Object?> get props => [note];
+}
+
+final class UpdateNoteRequested extends NotesEvent {
+  const UpdateNoteRequested(this.note);
+
+  final Note note;
+
+  @override
+  List<Object?> get props => [note];
+}
+
+final class DeleteNoteRequested extends NotesEvent {
+  const DeleteNoteRequested(this.noteId);
+
+  final String noteId;
+
+  @override
+  List<Object?> get props => [noteId];
+}
